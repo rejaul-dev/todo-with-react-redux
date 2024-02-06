7af350f12ca780c8ed51ccaa9cd799bb30973c7b
karimscoding <rejaulkarim66666@gmail.com>
2024-02-07 01:13:25 +0800

    ci: project setup completed⚡

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold">Todo Application</h1>
+      <Button>Redux</Button>
+    </>
+  );
+}
+
+export default App;
